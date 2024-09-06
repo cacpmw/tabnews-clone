d5800bc447047ba7c11e564d18b0eecd98187825
Carlos <carloscarneiro@uft.edu.br>
2024-09-06 16:08:23 +0000

    finalized /status endpoint along with integration test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,10 +2,38 @@
 import database from "/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
+  const databaseServerVersionResult = await database.query(
+    "SHOW server_version;"
+  );
+  const databaseVersionValue =
+    databaseServerVersionResult.rows[0].server_version;
 
-  response.status(200).json({ chave: "Ok" });
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  //sql sanitization
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
